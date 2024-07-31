5f365e713666149efc8ae58a828942935d6f1175
srinivassankoji <srinivaschary.chary@gmail.com>
2024-07-31 21:44:58 +0530

    Converted to Web Application

--- src/main/java/com/school/smart/SmartSchoolApplication.java
+++ src/main/java/com/school/smart/SmartSchoolApplication.java
@@ -2,9 +2,10 @@ package com.school.smart;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class SmartSchoolApplication {
+public class SmartSchoolApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SmartSchoolApplication.class, args);
