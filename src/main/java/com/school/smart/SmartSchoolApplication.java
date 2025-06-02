ec0aad3860ee6a9d01db8c96476d5d32bb77e045
srinivassankoji <srinivaschary.chary@gmail.com>
2025-06-02 14:38:36 +0530

    Added get method for welcme message

--- src/main/java/com/school/smart/SmartSchoolApplication.java
+++ src/main/java/com/school/smart/SmartSchoolApplication.java
@@ -3,12 +3,18 @@ package com.school.smart;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @SpringBootApplication
 public class SmartSchoolApplication extends SpringBootServletInitializer {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SmartSchoolApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(SmartSchoolApplication.class, args);
+    }
+
+    @GetMapping
+    public String getMessage() {
+        return "Smart school application";
+    }
 
 }
